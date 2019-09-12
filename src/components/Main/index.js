72bdf529f56c100bc2c22df702e85fb17f0952fb
Lucas Borges <lucas.borges@sonne-consulting.com>
2019-09-12 15:47:04 -0300

    adicionado dropdown com lista das cidades, testar mais tarde pois o limite de consultas gratuitas a api já estourou

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -7,35 +7,84 @@ import Item from './item';
 import SearchableDropdown from 'react-native-searchable-dropdown';
 
 const cities = [
+  {id: '0', name: 'Selecione uma cidade'},
   {id: '455827', name: 'São Paulo'},
+  {id: '447216', name: 'Rio Branco'},
+  {id: '90200707', name: 'Rio de Janeiro'},
+  {id: '455833', name: 'Manaus'},
+  {id: '455936', name: 'Boa Vista'},
+  {id: '455820', name: 'Belém'},
+  {id: '455820', name: 'Belém'},
+  {id: '455970', name: 'Macapá'},
+  {id: '455901', name: 'Porto Velho'},
+  {id: '457721', name: 'Palmas'},
+  {id: '455880', name: 'Maceió'},
 ];
 
 export default function index() {
-  const [climate, setClimate, city, setCity] = useState({});
+  const [climate, setClimate] = useState({});
+  const [city, setCity] = useState({});
   useEffect(() => {
     init();
   }, []);
 
   const init = async () => {
     try{
+      setCity({id: '455827', name: 'São Paulo'});
       const climateReceived = await api.get('455827');
       setClimate(climateReceived.data.results);
-      setCity({id: '455827', name: 'São Paulo'});
     }
     catch (error){
       alert(error);
     }
   };
 
+  const findCity = async (item) => {
+    try{
+      const climateReceived = await api.get(item.id);
+      setClimate(climateReceived.data.results);
+    }
+    catch (error){
+      alert(error);
+    }
+  }
+
   return(
     <View style={styles.container}>
-      <SearchableDropdown 
-        multi={false}
-        selectedItems={city}
-        onItemSelect={(item) => {
-          setCity(item);
-        }}
-      />
+        <SearchableDropdown 
+          selectedItems={[city]}
+          onItemSelect={(item) => {
+            if (item.id!=='0'){
+              setCity(item);
+              findCity(item);
+            }
+          }}
+          defaultIndex={1}
+          itemStyle={{
+            width: '96%',
+            height: 40,
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginLeft: '2%',
+            backgroundColor: '#ddd',
+            borderColor: '#bbb',
+            borderWidth: 1,
+            borderRadius: 5,
+          }}
+          textInputStyle={{
+            //inserted text style
+            width: '96%',
+            justifyContent: 'center',
+            padding: 5,
+            margin: '2%',
+            borderWidth: 1,
+            borderColor: '#ccc',
+            backgroundColor: '#FAF7F6',
+          }}
+          itemTextStyle={{ color: '#222', width: '100%', }}
+          itemsContainerStyle={{ maxHeight: 180, width: '100%', }}
+          items={cities}
+        />
       { climate.forecast &&
         <Item data={climate} forecast={climate.forecast} />
       }
